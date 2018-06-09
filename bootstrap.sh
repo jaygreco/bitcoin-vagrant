fb4e86b0bfab9e3a759fa586283e59accea3767f
Jay Greco <jayv.greco@gmail.com>
2018-06-09 10:42:37 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install -y git
 sudo apt-get install -y build-essential
 sudo apt-get install -y bsdmainutils
